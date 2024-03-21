96b5e8232853ae9a1b24e20b4ac1f67f52c898bd
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2024-03-21 12:50:15 +0000

    Add trim whitespace on save

---
+++ lua/core/autocommands.lua
@@ -0,0 +1,11 @@
+-- Trim all trailing whitespace
+vim.api.nvim_create_autocmd(
+  "BufWritePre",
+  {
+    pattern = "*",
+    callback = function()
+      vim.cmd([[ %s/\s\+$//e ]])
+    end,
+  }
+)
+
