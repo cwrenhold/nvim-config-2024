9cf96df6cef08bcc2354d76756845929f14dad11
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2024-04-01 09:27:13 +0100

    Move nvim-tree to lazy loading

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -2,11 +2,20 @@ return {
   "nvim-tree/nvim-tree.lua",
   -- Optional dependency
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  lazy = true,
+  keys = {
+    { "<leader>ex", "<cmd>NvimTreeToggle<CR>", desc = "Toggle [Ex]plorer" },
+  },
+  cmd = {
+    "NvimTreeFindFile",
+    "NvimTreeFindFileToggle",
+    "NvimTreeOpen",
+    "NvimTreeRefresh",
+    "NvimTreeToggle",
+  },
   config = function()
     require('nvim-tree').setup({
       hijack_netrw = false
     })
-
-    vim.keymap.set('n', '<leader>ex', ':NvimTreeFindFileToggle<CR>', { desc = 'Toggle [Ex]plorer' })
   end,
 }
