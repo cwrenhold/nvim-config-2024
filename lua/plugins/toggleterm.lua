5f06ae2db8ecb3c20d8b03733bec57443b8da679
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2024-04-07 17:13:14 +0100

    Add toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+  'akinsho/toggleterm.nvim',
+  opts = {
+    --[[ things you want to change go here]]
+  },
+  keys = {
+    {
+      "<leader>to",
+      "<cmd>ToggleTerm<CR>",
+      desc = "Toggle terminal"
+    }
+  }
+}
